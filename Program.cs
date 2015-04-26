0a421229ad3e37af804f41f3126c3d025c3066f7
Cameron Murphy <camurphy@gmail.com>
2015-04-26 16:52:43 +1000

    Wrote the code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Camurphy.CompletedTorrentOrganiser.Properties;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,29 @@ namespace Camurphy.CompletedTorrentOrganiser
     {
         static void Main(string[] args)
         {
+            string completedDownloadsDirectory = Settings.Default.CompletedDownloadsDirectory;
+
+            if (!completedDownloadsDirectory.EndsWith("\\")) {
+                completedDownloadsDirectory += "\\";
+            }
+
+            string[] paths = Directory.GetDirectories(completedDownloadsDirectory);
+
+            foreach (string path in paths)
+            {
+                string directoryName = Path.GetFileName(path);
+
+                string[] files = Directory.GetFiles(path);
+
+                foreach (string file in files.Where(f => Settings.Default.SupportedFileExtensions.Contains(Path.GetExtension(f.ToLower()))))
+                {
+                    string destination = completedDownloadsDirectory + directoryName + Path.GetExtension(file.ToLower());
+                    File.Move(file, destination);
+                    break;
+                }
+
+                Directory.Delete(path, true);
+            }
         }
     }
 }
